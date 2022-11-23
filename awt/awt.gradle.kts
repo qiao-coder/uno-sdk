ddbf94577ea1499642f037de2f3f27aa605aaaa0
qiao <1024522245@qq.com>
2022-11-23 10:24:19 +0800

    update dependency

--- awt/awt.gradle.kts
+++ awt/awt.gradle.kts
@@ -16,8 +16,9 @@ version = rootProject.version
 dependencies {
     implementation(projects.core)
 
-    implementation("kotlin.graphics:gln:0.5.31")
+//    implementation("kotlin.graphics:gln:0.5.31")
 //    implementation("kotlin.graphics:glm:0.9.9.1-5")
+    implementation("com.github.qiao-coder:gln:0.5.32")
     implementation("com.github.qiao-coder:glm:0.9.9.1-6")
     implementation("kotlin.graphics:kool:0.9.68")
 
--- core/core.gradle.kts
+++ core/core.gradle.kts
@@ -16,7 +16,7 @@ version = rootProject.version
 dependencies {
     implementation(kotlin("reflect"))
 
-    implementation("kotlin.graphics:gln:0.5.31")
+    implementation("kotlin.graphics:gln:0.5.32")
     implementation("kotlin.graphics:gli:0.8.3.0-18")
 //    implementation("kotlin.graphics:glm:0.9.9.1-5")
     implementation("com.github.qiao-coder:glm:0.9.9.1-6")
